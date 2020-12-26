8fb409cc2c0d46b7aaf3c6f158f5f38474ee3e78
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-12-26 17:58:16 +0100

    lcd screen now display message received from udp client

--- main.ino
+++ main.ino
@@ -47,14 +47,11 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Hello, World!");
+  Serial.begin(115200);
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  lcd.print(Serial.readString());
+  delay(500);
+  lcd.clear();
 }
