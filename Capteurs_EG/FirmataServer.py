34bcb6caa7d1e636c523c3a298436de12c2873f9
Remi-Guijarro <remi.guijarro@outlook.fr>
2021-01-20 19:15:35 +0100

    Added alarm to FirmataServer

--- Capteurs_EG/FirmataServer.py
+++ Capteurs_EG/FirmataServer.py
@@ -2,8 +2,10 @@
 import pyfirmata
 import time
 
-boardKeypad = pyfirmata.Arduino('/dev/cu.usbmodem4301')
-#boardRFID = pyfirmata.Arduino('/dev/cu.usbmodem4301')
+# boardKeypad = pyfirmata.Arduino('/dev/cu.usbmodem4301')
+# boardRFID = pyfirmata.Arduino('/dev/cu.usbmodem4301')
+alarmBoard = pyfirmata.arduino('COM4')
+
 print("Communication Successfully started")
 
 pos = 0
@@ -15,6 +17,9 @@ isCard = False
 
 isWet = False
 sensorLimit = 0.1
+
+isAlarmOn = False
+
 
 def on_key_received(*args, **kwargs):
     global test, pos, isVerified
@@ -38,6 +43,7 @@ def on_key_received(*args, **kwargs):
         pos = 0
         test = ['0', '0', '0', '0']
 
+
 def on_card_received(*args, **kwargs):
     global isCard
     if len(args) > 0:
@@ -47,8 +53,19 @@ def on_card_received(*args, **kwargs):
             print("Open")
 
 
+def trigger_alarm():
+    alarmBoard.digital[3].mode = pyfirmata.OUTPUT  # set digital pin to output mode
+    alarmBoard.digital[5].mode = pyfirmata.OUTPUT
+    BUZZER_PIN = alarmBoard.get_pin('d:3:p')  # set digital pin 3 to pwn communication
+    BUZZER_PIN.write(0.5)  # analog write equivalent
+    alarmBoard.digital[5].write(1)  # digital write equivalent
+    time.sleep(1)
+    BUZZER_PIN.write(0.1)
+    alarmBoard.digital[5].write(0)
+
+
 if __name__ == '__main__':
-    a0 = boardKeypad.get_pin('a:0:i')
+    #a0 = boardKeypad.get_pin('a:0:i')
         
     itKeypad = pyfirmata.util.Iterator(boardKeypad)
     itKeypad.start()
@@ -56,7 +73,7 @@ if __name__ == '__main__':
     #itRFID = pyfirmata.util.Iterator(boardRFID)
     #itRFID.start()
     
-    boardKeypad.add_cmd_handler(pyfirmata.pyfirmata.STRING_DATA, on_key_received)
+    #boardKeypad.add_cmd_handler(pyfirmata.pyfirmata.STRING_DATA, on_key_received)
     #boardRFID.add_cmd_handler(pyfirmata.pyfirmata.STRING_DATA, on_card_received)
     
     
@@ -68,9 +85,11 @@ if __name__ == '__main__':
             #boardKeypad.send_sysex(0x08, [])
         #if isCard == False:
             #boardRFID.send_sysex(0x09, [])
-        if isWet == False:
-            sensorValue = a0.read()
-            print(sensorValue)
-            if sensorValue > sensorLimit:
-                print("in_water")
-                isWet = True
+        #if isWet == False:
+            #sensorValue = a0.read()
+            #print(sensorValue)
+            #if sensorValue > sensorLimit:
+            #    print("in_water")
+            #    isWet = True
+        if isAlarmOn:
+            trigger_alarm()
